8161bfc80ef541abb0f9e6eddada3bdb3d2913f4
JarvisChan666 <744765386@qq.com>
2023-11-17 21:33:43 +0800

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'// 引入path模块
 // https://vitejs.dev/config/
-export default {
-    base: './'
-    plugins: [
-        vue(),
-    ],
-};
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+});
