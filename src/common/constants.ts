09ae899c95aff52e16c0fe1fabbdf20f37af22d4
JarvisChan666 <744765386@qq.com>
2023-11-27 16:22:30 +0800

    feat(constants): define user-related constants
    - Define URLs for user login, logout, registration, and info retrieval
    - Define constants for user account, user password, check password, and planet code

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,7 +1,9 @@
 export const LOGIN_URL = '/user/login';
+export const LOGOUT_URL = '/user/logout';
 export const REGISTER_URL = '/user/register';
 export const CURRENT_USER_URL = '/user/current';
 export const UPDATE_USER_URL = '/user/update';
+export const USER_INFO_URL = '/user/info';
 export const USER_ACCOUNT = 'userAccount';
 export const USER_PASSWORD = 'userPassword';
 export const CHECK_PASSWORD = 'checkPassword';
