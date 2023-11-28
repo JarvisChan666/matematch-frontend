787c7846137ecf3760e6dc47b48aed7ae421c344
JarvisChan666 <744765386@qq.com>
2023-11-28 18:17:03 +0800

    feat(*): add githubpage configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'; // 引入path模块
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// base: '/matematch-frontend/',
+	base: '/matematch-frontend/',
 	// publicPath: './',
 	plugins: [vue()],
 	build: {
