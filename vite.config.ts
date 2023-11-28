d4e7444f8aee39eb762f67fc5657f5be69565120
JarvisChan666 <744765386@qq.com>
2023-11-28 16:10:34 +0800

    fix: fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue'; // 引入path模块
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	// base: '/matematch-frontend/',
+	base: '/matematch-frontend/',
 	// publicPath: './',
 	plugins: [vue()],
 });
