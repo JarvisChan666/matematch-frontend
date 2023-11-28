65cd66880cd7b53e6c4791bd0acd2b22a9ec1f60
JarvisChan666 <744765386@qq.com>
2023-11-28 18:24:28 +0800

    feat(*): add githubpage configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,8 @@ import { routes } from './routes';
 // Create a router
 // histroy模式
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory(process.env.BASE_URL || '/matematch-frontend/'),
+	// history: createWebHistory(),
 	routes,
 });
 // 为了给不是vue文件组合式api，比如ts文件中用router，这里将其实例化并导出
