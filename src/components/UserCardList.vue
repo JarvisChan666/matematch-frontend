cd75bd53fd5c19edccd0b900e1d8bf9e81451d60
JarvisChan666 <744765386@qq.com>
2023-11-30 15:24:08 +0800

    feat(UserCardList): extract reusable component
    Extracted UserCardList component for reusability. This component displays a list of user cards, each with user information and actions. It also includes a feature to show user's email.

---
+++ src/components/UserCardList.vue
@@ -0,0 +1,41 @@
+<template>
+	<van-card
+		v-for="(user, index) in userList"
+		:desc="user.profile"
+		:title="`${user.username} (${user.planetCode})`"
+		:thumb="user.avatarUrl"
+		style="padding-bottom: 50px;"
+	>
+		<template #tags>
+			<van-tag v-for="tag in tags" plain type="primary" style="margin-top: 8px">{{ tag }}</van-tag>
+		</template>
+		<template #footer>
+			<van-popover :actions="actions" @select="onSelect">
+				<template #reference>
+					<!--          TODO 展示用户邮箱-->
+					<van-button type="primary" @click="() => showEmail(index)">TODO获取邮箱</van-button>
+				</template>
+			</van-popover>
+		</template>
+	</van-card>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps } from 'vue';
+import { userType } from '../models/user';
+import { showToast } from 'vant';
+
+interface UserCardListProps{
+  userList: userType[]
+}
+
+// withDefault让props有个默认值空数组
+const props = withDefaults(defineProps<UserCardListProps>(), {
+  userList: [] as userType[],
+})
+
+const showEmail = (index: number) => {
+    const user = props.userList[index];
+    showToast(user.email);
+};
+</script>
