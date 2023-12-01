cc961c86cd8ff544c606a7ef2b066869c054ce46
JarvisChan666 <744765386@qq.com>
2023-12-01 16:32:30 +0800

    feat(SearchResultPage.vue): add skeleton screen for UserCardList

--- src/pages/SearchResultPage.vue
+++ src/pages/SearchResultPage.vue
@@ -1,7 +1,21 @@
 <template>
 	<!--  TODO 反馈组件添加：Loading加载中。下拉刷新，展示组件骨架屏-->
-	<UserCardList :user-list="userList"/>
-	<van-empty v-if="!userList || userList.length < 1" description="搜索结果为空" />
+	<van-skeleton :loading="loading" title :row="3">
+		<UserCardList :user-list="userList" />
+		<template #template>
+			<div :style="{ display: 'flex', width: '100%' }">
+				<van-skeleton title avatar avatar-size="80px" :row="3" />
+				<div :style="{ flex: 1, marginLeft: '16px' }">
+					<van-skeleton-paragraph row-width="60%" />
+					<van-skeleton-paragraph />
+					<van-skeleton-paragraph />
+					<van-skeleton-paragraph />
+				</div>
+			</div>
+		</template>
+	</van-skeleton>
+	<!-- <UserCardList :user-list="userList" /> -->
+	<!-- <van-empty v-if="!userList || userList.length < 1" description="搜索结果为空" /> -->
 </template>
 
 <script setup>
@@ -11,6 +25,8 @@ import myAxios from '../plugins/myAxios.ts';
 import qs from 'qs';
 import { showToast } from 'vant';
 import UserCardList from '../components/UserCardList.vue';
+import { component as VueLazyComponent } from '@xunlei/vue-lazy-component';
+import { Skeleton } from 'vant';
 
 const showPopover = ref(false);
 
@@ -25,40 +41,45 @@ const showEmail = () => {
 	showToast('提示内容');
 };
 
+let loading = ref(true);
+
 const tags = route.query.tags ? route.query.tags.split(',') : [];
 
-const fetchUserList = async() => {
-	 try {
-    const response = await myAxios.get('/user/search/tags', {
-      params: {
-        tagNameList: tags,
-      },
-      paramsSerializer: {
-        serialize: (params) => qs.stringify(params, { indices: false }),
-      },
-    });
+const fetchUserList = async () => {
+	try {
+		const response = await myAxios.get('/user/search/tags', {
+			params: {
+				tagNameList: tags,
+			},
+			paramsSerializer: {
+				serialize: (params) => qs.stringify(params, { indices: false }),
+			},
+		});
 
-    console.log('/user/search/tags succeed', response);
+		console.log('/user/search/tags succeed', response);
 
-    const userListData = response.data?.data;
+		const userListData = response.data?.data;
 
-    if (userListData) {
-      userListData.forEach((user) => {
-        if (user.tags) {
-          user.tags = JSON.parse(user.tags);
-        }
-      });
-      userList.value = userListData;
-    }
-  } catch (error) {
-    console.log('/user/search/tags error', error);
+		if (userListData) {
+			userListData.forEach((user) => {
+				if (user.tags) {
+					user.tags = JSON.parse(user.tags);
+				}
+			});
+			userList.value = userListData;
+		}
+	} catch (error) {
+		console.log('/user/search/tags error', error);
 		showToast('Failed to fetch user list');
-    // Add more error handling logic here
-  }
-}
+		// Add more error handling logic here
+	}
+};
 
 onMounted(async () => {
-	fetchUserList();
+	// 等获取完后才设置为加载结束
+	await fetchUserList();
+	// 当数据加载完成后，将 loading 设置为 false
+	loading.value = false;
 });
 </script>
 
