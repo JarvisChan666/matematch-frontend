dbefdfe3929f4d509fe41c18469a6b682d9dfee4
JarvisChan666 <744765386@qq.com>
2023-12-01 15:21:53 +0800

    feat(user.ts): use Object.freeze to improve performance

--- src/store/user.ts
+++ src/store/user.ts
@@ -35,7 +35,7 @@ const useUserStore = defineStore('user', {
 				const { data, description } = res.data;
 				// 用data判断，后端登录失败还是返回code=0
 				if (data) {
-					this.userInfo = data;
+					this.userInfo = Object.freeze(data);
 					showSuccessToast('登录成功');
 					return true;
 				} else {
@@ -53,9 +53,10 @@ const useUserStore = defineStore('user', {
 				const res = await axios.post(REGISTER_URL, userData);
 				const { data, code, description } = res.data;
 				if (code === 0) {
-					this.userInfo = data;
+					// 使用Object.freeze来冻结userInfo对象。这样可以防止Vue对userInfo进行不必要的数据劫持，从而提高性能。
+					this.userInfo = Object.freeze(data);
+					// this.userInfo = data;
 					showSuccessToast('注册成功');
-					// localStorage.setItem('user', JSON.stringify(data));
 					return true;
 				} else {
 					showFailToast('注册失败 ' + description);
@@ -89,13 +90,20 @@ const useUserStore = defineStore('user', {
 
 		async getCurrentUser() {
 			try {
+				const cachedData = localStorage.getItem('userInfo');
+				if (cachedData) {
+					const userInfo = JSON.parse(cachedData);
+					console.log('get current user from localStorage' + userInfo);
+					return userInfo;
+				}
 				const res = await axios.get(CURRENT_USER_URL);
 				const { data, code } = res.data;
-				console.log('get current user' + data);
 				if (code === 0) {
 					// 前端只要文字，后端只要数字，两边通信时先转成对方需要的(这里都给前端处理)
 					// Map gender from 0 and 1 to '男' and '女'
 					data.gender = data.gender === 0 ? '男' : '女';
+					localStorage.setItem('userInfo', JSON.stringify(data));
+					console.log('get current user from axios' + data);
 					return data;
 				} else {
 					// 只有当当前路由不是登录路由时，才显示失败的 Toast 消息
@@ -119,6 +127,7 @@ const useUserStore = defineStore('user', {
 			// 是否更新用户用put请求？
 			const res = await axios.post(UPDATE_USER_URL, updateUser);
 			if (res.data.code === 0) {
+				localStorage.removeItem('userInfo');
 				this.userInfo = await this.getCurrentUser();
 				showSuccessToast('更新信息成功');
 			} else {
